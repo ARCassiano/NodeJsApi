827450e4d025785f36f97fb9d91bc51136087958
ARCassiano <anderson.cassiano@hotmail.com>
2019-02-16 19:17:22 -0200

    Primeira API Node.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 const http = require("http");
+const status = require("http-status");
 const express = require("express");
+const spoilersRoute = require("./routes/spoiler");
+const sequelize = require("./database/database");
+
 
 const app = express();
 
-const hostname = "127.0.0.1";
-const port = 3000;
+app.use(express.json());
 
-app.set("port", port);
+app.use('/api', spoilersRoute);
 
 app.use((request, response, next) => {
-    response.status(404).send();
+    response.status(status.NOT_FOUND).send();
 });
 
-const server = http.createServer(app);
+app.use((error, request, response, next) => {
+    response.status(status.INTERNAL_SERVER_ERROR).json({ error });
+});
 
-server.listen(port, hostname, () => {
-    console.log('Servidor em execução em http://${hostname}:${port}/');
+sequelize.sync({ force: false }).then(() => {
+    const port = process.env.port || 3000;
+
+    app.set("port", port);
+
+    const server = http.createServer(app);
+
+    server.listen(port);
 });
